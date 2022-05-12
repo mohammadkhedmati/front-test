62e577d5a430e702f90734fc992e11fbc09776ae
mohammadkhedmati <mohammad.khedmati2012@gmail.com>
2022-05-12 12:25:33 +0430

    countdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react'
-import Body from "./components/body";
 import Header from "./components/header";
 import Fotter from "./components/footer";
 import './App.css';
 import Sidebar from './components/sidebar';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { number: 500 };
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  componentDidMount() {
+    this.startTimer()
+  }
+
+  startTimer() {
+    this.timer = setInterval(this.countDown, 1000);
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let number = this.state.number - 1;
+    this.setState({
+      number: number,
+    });
+    
+    // Check if we're at zero.
+    if (number === 0) { 
+      clearInterval(this.timer);
+    }
+  }
+
   render() {
     return (
       <div className='App'>
         <Sidebar />
-        <Header />
-        <Body />
+        <Header time={this.state.number}/>
+        <div className='main'>
+          <div>
+            countDown: {this.state.number} 
+          </div>
+        </div>
         <Fotter />
       </div>
     )
